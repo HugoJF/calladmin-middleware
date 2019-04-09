266143f1bd4d610b1e8f9900aebac36791d10e90
Hugo Jeller <hugo_jeller@hotmail.com>
2019-04-09 04:05:20 -0400

    added csgo-server-api-laravel interface

--- app/Listeners/TriggerGotvRecording.php
+++ app/Listeners/TriggerGotvRecording.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\ReportCreated;
+use hugojf\CsgoServerApi\Facades\CsgoApi;
 use Ixudra\Curl\Facades\Curl;
 
 class TriggerGotvRecording
@@ -40,17 +41,20 @@ class TriggerGotvRecording
 	{
 		$this->report = $reportCreated->report;
 
-		$this->sendCommand($this->startCommand . ' demos/' . $this->report->demoFilename);
-		$this->sendCommand('sm_csay Uma DEMO será armazenada em 10 segundos, o servidor travará brevemente devido a uma limitação da Source Engine e não pode ser evitado. Utilize os reports apenas quando necessário!', 1000);
-		$this->sendCommand('sm_csay Uma DEMO será armazenada em 5 segundos, o servidor travará brevemente devido a uma limitação da Source Engine e não pode ser evitado. Utilize os reports apenas quando necessário!', 6000);
-		$this->sendCommand('sm_csay Uma DEMO será armazenada em 3 segundos, o servidor travará brevemente devido a uma limitação da Source Engine e não pode ser evitado. Utilize os reports apenas quando necessário!', 9000);
-		$this->sendCommand('sm_csay Uma DEMO será armazenada em 2 segundos, o servidor travará brevemente devido a uma limitação da Source Engine e não pode ser evitado. Utilize os reports apenas quando necessário!', 10000);
-		$this->sendCommand('sm_csay Uma DEMO será armazenada em 1 segundo, o servidor travará brevemente devido a uma limitação da Source Engine e não pode ser evitado. Utilize os reports apenas quando necessário!', 11000);
-		$this->sendCommand('sm_csay DEMO iniciada!', 12500);
-		$this->sendCommand($this->stopCommand, 13000);
-		$this->sendCommand('sm_csay DEMO finalizada!', 14000);
-		$this->sendCommand('sm_say O resultado do report poderá ser consultado em:', 15000);
-		$this->sendCommand('sm_say "' . route('reports.show', $this->report) . '"', 15500);
+		CsgoApi::to($this->report->server_ip . ':' . $this->report->server_port)
+			   ->execute([
+				   [$this->startCommand . ' demos/' . $this->report->demoFilename, 500],
+				   ['sm_csay Uma DEMO será armazenada em 10 segundos, o servidor travará brevemente devido a uma limitação da Source Engine e não pode ser evitado. Utilize os reports apenas quando necessário!', 1000],
+				   ['sm_csay Uma DEMO será armazenada em 5 segundos, o servidor travará brevemente devido a uma limitação da Source Engine e não pode ser evitado. Utilize os reports apenas quando necessário!', 6000],
+				   ['sm_csay Uma DEMO será armazenada em 3 segundos, o servidor travará brevemente devido a uma limitação da Source Engine e não pode ser evitado. Utilize os reports apenas quando necessário!', 9000],
+				   ['sm_csay Uma DEMO será armazenada em 2 segundos, o servidor travará brevemente devido a uma limitação da Source Engine e não pode ser evitado. Utilize os reports apenas quando necessário!', 10000],
+				   ['sm_csay Uma DEMO será armazenada em 1 segundo, o servidor travará brevemente devido a uma limitação da Source Engine e não pode ser evitado. Utilize os reports apenas quando necessário!', 11000],
+				   ['sm_csay DEMO iniciada!', 12500],
+				   [$this->stopCommand, 13000],
+				   ['sm_csay DEMO finalizada!', 14000],
+				   ['sm_say O resultado do report poderá ser consultado em:', 15000],
+				   ['sm_say "' . route('reports.show', $this->report) . '"', 15500],
+			   ])->run();
 	}
 
 	public function sendCommand($command, $delay = 0)
---
+++ config/csgo-api.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+	'url' => env('CSGO_API_URL'),
+	'key' => env('CSGO_API_KEY'),
+];
