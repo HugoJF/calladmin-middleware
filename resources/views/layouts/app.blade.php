fdd3e8d6ded32974dd908ef5476bf08c8240d8e1
Hugo Jeller <hugo_jeller@hotmail.com>
2020-02-07 00:47:42 +0000

    Improved ackknowledgement screen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,9 @@
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
+    <!-- AlpineJS -->
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v1.9.8/dist/alpine.js" defer></script>
+
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
@@ -119,6 +122,6 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
 @stack('scripts')
+</body>
 </html>
--- resources/views/reports/ack-text.blade.php
+++ resources/views/reports/ack-text.blade.php
@@ -2,12 +2,12 @@
     <div class="col">
         <h3 class="d-block mb-4">Por favor leia todo o texto abaixo antes de continuar:</h3>
         <p class="text-muted">Sem pressa, você terá que esperar 1min nessa página antes de aceitar.</p>
-        
+
         <h4 class="mt-5">► O que é isso?</h4>
         <p>Visando melhorar a experiência de jogo em todos os nossos servidores, <strong>estamos implementando novas regras e protocolos em nosso sistema de report</strong> com objetivo de reduzir a quantidade de falsos-negativos e permitir que nossa equipe tenha o menor tempo de resposta possível.</p>
-        
+
         <p>Começando em Maio de 2019, <strong>todos os jogadores que enviarem reports incorretos</strong>, terão que passar por essa confirmação em nosso sistema <strong>para CADA report incorreto submetido ao sistema.</strong></p>
-        
+
         <h4 class="mt-5">► Por quê?</h4>
         <p>
             Com o constante crescimento de nossa rede de servidores, encontramos um grande desafio: <strong>lidar com a quantidade de reports</strong>.
@@ -19,10 +19,10 @@
         <p>
             O objetivo de bloquear o jogador temporariamente é <strong>criar uma penalidade associada ao uso incorreto da ferramente de reports de forma a desestimular reports sem motivo</strong>
         </p>
-        
+
         <h4 class="mt-5">► O que devo fazer para poder jogar novamente?</h4>
         <p class="para-de-querer-copiar-e-colar-seu-puto-e-aprende-que-nao-eh-pra-ficar-reportando-qualquer-um-na-porra-dos-servidores">Por favor digite abaixo e clique em confirmar: <strong>{{ \App\Http\Controllers\MyReportsController::CONFIRMATION_TEXT }}</strong></p>
-        
+
         <h4 class="mt-5">► Nosso objetivo</h4>
         <ul>
             <li>Reduzir a <strong>quantidade de reports errados</strong> no sistema;</li>
@@ -30,7 +30,7 @@
             <li>Tornar nossos <strong>reports mais úteis</strong> para outros servidores;</li>
             <li><strong>Melhorar a <i>quality-of-life</i></strong> de todos os nossos servidores.</li>
         </ul>
-        
+
         <h4 class="mt-4">► Como posso melhorar</h4>
         <p>É extremamente simples:</p>
         <ul>
@@ -40,24 +40,91 @@
             <li>Não reporte ninguém <strong>se estiver irritado</strong> com o jogo.</li>
         </ul>
         <p>Caso tenha interesse na lógica atrás dessa ferramenta e detalhes sobre os desafios de hospedar servidores de CS:GO, <a href="https://denerdtv.com/confirmacoes-de-reports-errados">clique aqui para ler nosso post a respeito disso.</a></p>
-        
+
         <h4 class="mt-4">► TL;DR</h4>
-        
-        <p><strong>Você enviou um report incorreto para nossos sistemas, e está temporariamente bloqueado de jogar em todos os nossos servidores até concordar em ser mais criterioso com os reports.</strong></p>
-        @auth
-            <div>
+
+        <p>Você enviou um report incorreto para nossos sistemas, e está temporariamente bloqueado de jogar em todos os nossos servidores até concordar em ser mais criterioso com os reports.</p>
+
+        <p><strong>Digite o texto abaixo no campo de verificação:</strong></p>
+
+        <div x-data="differ()" x-init="update()">
+            <div class="para-de-querer-copiar-e-colar-seu-puto-e-aprende-que-nao-eh-pra-ficar-reportando-qualquer-um-na-porra-dos-servidores">
+                <pre class="d-flex justify-content-center">
+                    <template x-for="part in parts">
+                        <span
+                            x-text="part.text"
+                            :class="{
+                                'text-success': part.type,
+                                'font-weight-bold text-danger': !part.type,
+                            }"
+                        ></span>
+                    </template>
+                </pre>
+            </div>
+
+            <p class="text-center font-weight-bold" x-show="value === original">Obrigado :)</p>
+
+            @auth
                 <form action="{{ route('my-reports.acked', $report) }}" method="POST" class="form-inline">
                     @csrf
                     <div class="form-group mx-sm-3 mb-2 flex-grow-1">
                         <label for="ack" class="sr-only">Ack</label>
-                        <textarea style="height: 84px;" name="confirmation" class="form-control-lg w-100" id="ack" placeholder="{{ \App\Http\Controllers\MyReportsController::CONFIRMATION_TEXT }}"></textarea>
+                        <textarea
+                            x-on:keyup="update()"
+                            x-model="value"
+                            style="height: 84px;"
+                            name="confirmation"
+                            class="form-control-lg w-100"
+                            id="ack"
+                            placeholder="{{ \App\Http\Controllers\MyReportsController::CONFIRMATION_TEXT }}"
+                        ></textarea>
                     </div>
-                    <button type="submit" class="btn btn-lg btn-primary mb-2">Confirmar</button>
+                    <button
+                        :disabled="value !== original"
+                        :class="{
+                            'btn-success': value === original,
+                            'btn-outline-dark': value !== original
+                        }"
+                        type="submit"
+                        class="btn btn-lg mb-2"
+                    >Confirmar
+                    </button>
                 </form>
-            </div>
-        @endauth
+            @endauth
+        </div>
         @guest
             <h1 class="text-center">Por favor faça o login para realizar o processo de desbloqueio!</h1>
         @endguest
     </div>
 </div>
+
+@push('scripts')
+    <script type="application/javascript">
+        console.log('scripts');
+
+        function differ() {
+            return {
+                value: "",
+                original: "{{ \App\Http\Controllers\MyReportsController::CONFIRMATION_TEXT }}",
+                parts: [],
+                update() {
+                    let original = this.original;
+                    let comp = this.value;
+                    let ref = original.split("").map((val, i) => comp[i] === val);
+
+                    this.parts = [];
+
+                    for (let i = 0; i < original.length; i++) {
+                        if (i === 0 || ref[i - 1] !== ref[i]) {
+                            this.parts.push({
+                                text: "",
+                                type: ref[i]
+                            });
+                        }
+                        this.parts[this.parts.length - 1].text += original[i];
+                    }
+                }
+            };
+        }
+    </script>
+@endpush
