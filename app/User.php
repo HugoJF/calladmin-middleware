cbcc7bfed8a962a5ed20ec178aefb5720aaeafd9
Hugo Jeller <hugo_jeller@hotmail.com>
2020-06-10 09:40:03 +0000

    Remove inline ide-helper

--- app/User.php
+++ app/User.php
@@ -5,18 +5,6 @@ namespace App;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-/**
- * App\User
- *
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Report[]                                                    $reports
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Report[]                                                    $targets
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Vote[]                                                      $votes
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
- * @mixin \Eloquent
- */
 class User extends Authenticatable
 {
     use Notifiable;
